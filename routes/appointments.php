4f6c251ff66ea3cc143ec092df094cd03db78587
mlmejo <mlmejo12@gmail.com>
2024-01-15 05:34:49 +0800

    remove duplicate named route

--- routes/appointments.php
+++ routes/appointments.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AppointmentController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AppointmentController::class)->group(function () {
-    Route::get('/', 'create')->name('appointments.show');
+    Route::get('/', 'create');
     Route::post('/appointments', 'store')->name('appointments.store');
     Route::get('/appointments/{appointment}', 'show')->name('appointments.show');
 });
